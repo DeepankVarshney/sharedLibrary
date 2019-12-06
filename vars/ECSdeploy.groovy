0d37452d52be7b8e0f1fb874bae0f06760056612
DeepankVarshney <deepankvarshney@gmail.com>
2019-12-06 12:45:36 +0530

    testing inputs

--- vars/ECSdeploy.groovy
+++ vars/ECSdeploy.groovy
@@ -8,9 +8,6 @@ def call(Map params) {
         tools {
             maven "maven_3.6"
         }
-        parameters {
-            string defaultValue: 'master', description: '', name: 'GIT_BRANCH', trim: true
-        }
 
         environment {
             dockerImage = ''
